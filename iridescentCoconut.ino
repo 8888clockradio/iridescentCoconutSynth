b0178cdaa469db5e65c83ab95058557323f8b16d
georgebeastiebach <grosar@georgerosar.com>
2020-11-14 08:56:32 -0600

    .78

--- iridescentCoconut.ino
+++ iridescentCoconut.ino
@@ -349,12 +349,14 @@ void loop() {
   if (bypassInstrumentMode) {
     betweenMixer1.gain(0, 0.0);
     betweenMixer2.gain(0, 0.0);
-    MasterOut1.gain(3, 1.0);
-    MasterOut2.gain(3, 1.0);
+    MasterOut1.gain(3, 0.78);
+    MasterOut2.gain(3, 0.78);
   }
   else {
-    betweenMixer1.gain(0, 1.0);
-    betweenMixer2.gain(0, 1.0);
+    //betweenMixer1.gain(0, 1.0);
+    //betweenMixer2.gain(0, 1.0);
+    betweenMixer1.gain(0, 0.0);
+    betweenMixer2.gain(0, 0.0);
     MasterOut1.gain(3, 0.0);
     MasterOut2.gain(3, 0.0);
   }
