9e4940ecdd4137d425e34e97afea34ca2320544e
georgebeastiebach <grosar@georgerosar.com>
2020-11-13 23:23:45 -0600

    update serial

--- name.c
+++ name.c
@@ -20,8 +20,6 @@ limitations under the License.
 #define MIDI_NAME   {'i','r','i','d','e','s','c','e','n','t',' ','C','o','c','o','n','u','t'}
 #define MIDI_NAME_LEN  18
 
-// Do not change this part.  This exact format is required by USB.
-
 struct usb_string_descriptor_struct usb_string_product_name = {
         2 + MIDI_NAME_LEN * 2,
         3,
@@ -38,5 +36,5 @@ struct usb_string_descriptor_struct usb_string_serial_number = {
   12,
   3,
   {
-    0,0,7,0,0,0,7,0,0,7  }
+    0,8,7,8,9,8,7,0,8,7  }
 };
