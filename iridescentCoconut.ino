2c8ec812f488234d880e12ada3af68db5b600fb7
88:88 Clock Radio <grosar@georgerosar.com>
2020-12-08 08:13:26 -0600

    Update iridescentCoconut.ino
    added de/re-tuning

--- iridescentCoconut.ino
+++ iridescentCoconut.ino
@@ -1,4 +1,3 @@
-
 /*
         Copyright 2020 George C. Rosar II licensed under:
                  Apache License
@@ -17,9 +16,56 @@ limitations under the License.
 #include <SPI.h>
 #include <SD.h>
 #include <SerialFlash.h>
-//#include <log2.h>  //this was one used to generate MIDI array to calculation hz from 440 HZ
 #include "constsAndStructs.h"
 #include "iridescentBasicSynth.h"
+
+
+//change the synths tuning
+//change the line reading '#define A4PITCH 440.0" to set the tuning
+//#define CHANGEPITCH       //COMMENT TO LEAVE A4 as CONCERT PITCH FOR A-440 //UNCOMMENT TO DE/RE-TUNE //JUST UNCOMMENTING THIS LINE WILL TUNE TO PHILOSOPHERS PITCH 432.0
+
+#ifdef CHANGEPITCH
+float midiNotes[128];
+//#define A4PITCH 440.0     //CONCERT A PITCH
+
+#define A4PITCH 432.0       //PHILOSOPHERS PITCH
+//#define A4PITCH 439.33    //ODD DETUNING JUST TO BE WEIRD
+#else                       //USES STANDARD 440 CONCERT PITCH IF CHANGEPITCH IS NOT DEFINED
+float midiNotes[128] = {8.18, 8.66, 
+    9.18, 9.72, 10.30, 10.91, 
+    11.56, 12.25, 12.98, 13.75, 
+    14.57, 15.43, 16.35, 17.32, 
+    18.35, 19.45, 20.60, 21.83, 
+    23.12, 24.50, 25.96, 27.50, 
+    29.14, 30.87, 32.70, 34.65, 
+    36.71, 38.89, 41.20, 43.65, 
+    46.25, 49.00, 51.91, 55.00, 
+    58.27, 61.74, 65.41, 69.30, 
+    73.42, 77.78, 82.41, 87.31, 
+    92.50, 98.00, 103.83, 110.00, 
+    116.54, 123.47, 130.81, 138.59, 
+    146.83, 155.56, 164.81, 174.61, 
+    185.00, 196.00, 207.65, 220.00, 
+    233.08, 246.94, 261.63, 277.18, 
+    293.66, 311.13, 329.63, 349.23, 
+    369.99, 392.00, 415.30, 440.00, 
+    466.16, 493.88, 523.25, 554.37, 
+    587.33, 622.25, 659.26, 698.46, 
+    739.99, 783.99, 830.61, 880.00, 
+    932.33, 987.77, 1046.50, 1108.73, 
+    1174.66, 1244.51, 1318.51, 1396.91, 
+    1479.98, 1567.98, 1661.22, 1760.00, 
+    1864.65, 1975.53, 2093.00, 2217.46, 
+    2349.32, 2489.02, 2637.02, 2793.83, 
+    2959.96, 3135.96, 3322.44, 3520.00, 
+    3729.31, 3951.07, 4186.01, 4434.92, 
+    4698.64, 4978.03, 5274.04, 5587.65, 
+    5919.91, 6271.93, 6644.88, 7040.00, 
+    7458.62, 7902.13, 8372.02, 8869.84, 
+    9397.27, 9956.06, 10548.08, 11175.30, 
+    11839.82, 12543.86,
+};
+#endif
 
 //#define DEBUG_ALLOC
 #define TEENSY_41_PINS      //use TEENSY 4_1 PINS rather than 3_6
@@ -349,6 +395,16 @@ void setup() {
   //#endif //DEBUG_ALLOC 
   delay(2000);
 
+#ifdef CHANGEPITCH
+  //calculate pitch
+  for (int i = 0; i < 128; i++) {
+    midiNotes[i] = (float)(( (float)A4PITCH) * (float)pow((float)2.0, ((float(i) - (float)69.0) / (float)12.0)));
+    Serial.printf("%f, ", midiNotes[i]);
+  }
+  Serial.printf("\r\n");
+  //midiNotes[];
+#endif
+
   pinMode(28, INPUT_PULLUP);
   pinMode(29, INPUT_PULLUP);
   pinMode(30, INPUT_PULLUP);
@@ -412,8 +468,8 @@ void setup() {
 
   //////
   //create Synth Object
-  synth1 = new iridescentBasicSynth(&synth1MasterOut1, &synth1MasterOut2, &button0, &button1, &button2, redPin, greenPin, bluePin, ledPin, ledPin2, &bypassInstrumentMode);
-  synth2 = new iridescentBasicSynth(&synth2MasterOut1, &synth2MasterOut2, &button0, &button1, &button2, redPin, greenPin, bluePin, ledPin, ledPin2, &bypassInstrumentMode);
+  synth1 = new iridescentBasicSynth(midiNotes, &synth1MasterOut1, &synth1MasterOut2, &button0, &button1, &button2, redPin, greenPin, bluePin, ledPin, ledPin2, &bypassInstrumentMode);
+  synth2 = new iridescentBasicSynth(midiNotes, &synth2MasterOut1, &synth2MasterOut2, &button0, &button1, &button2, redPin, greenPin, bluePin, ledPin, ledPin2, &bypassInstrumentMode);
   //////
   
   /////
